65d8add910c7d6c5ef02f3696cf1a2afc5979ea3
camiloisme <camiloesyo@gmail.com>
2024-11-14 06:32:00 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const navToggle = document.querySelector(".nav-toggle")
 const navBar = document.querySelector(".navbar")
 navToggle.addEventListener ("click", () => {
     navBar.classList.toggle("nav_active");
-})
+    if (navBar.classList.contains("nav_active")){
+        navToggle.setAttribute("arial-label", "Cerrar Menú");
+        }
+        else {
+            navToggle.setAttribute("arial-label", "Abrir Menú");
+        }
+      
+    }
+)
